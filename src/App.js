e39c9ea1308ea6782ce1b45753f47411d8280fa1
UndefinedLove <factknowledgeonly@gmail.com>
2024-04-23 13:41:58 +0530

    Created Basic File Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Authentication from './screens/Authentication';
+import Signin from './components/Authentication/Signin';
+import Signup from './components/Authentication/Signup';
+
+import Home from './screens/Home';
 
 function App() {
   return (
-   <>
-   Lets start the project
-   lets begin
-   </>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} >
+            {/* Inner Routes to be inserted here */}
+          </Route>
+
+          <Route path="/auth" element={<Authentication />}>
+            <Route index element={<Signin />} />
+            <Route path="/auth/signup" element={<Signup />} />
+          </Route>
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
