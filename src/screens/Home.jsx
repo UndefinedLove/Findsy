2a9299a3858b28777d7a0f97d94ef67c5b6a2782
UndefinedLove <factknowledgeonly@gmail.com>
2024-04-27 11:59:49 +0530

    Fixed the Push Pull workings (profDetails, Profile, home)

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,19 @@
 import { getAuth } from 'firebase/auth'
 import React from 'react'
 import { app } from '../firebase'
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
+  const currentUser = getAuth(app).currentUser;
+  const uid = currentUser ? currentUser.uid : null;
+
   return (
-    <div>{ (getAuth(app).currentUser.uid)}</div>
+    <>
+
+      <div>{uid}</div>
+      <Link to={'/profD'}>Click here</Link>
+    </>
   )
 }
 
